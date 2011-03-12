45d7e60f21faee0d77a58425b72d20d09652982f
Isaac Lewis <isaac@isaac-dell-laptop.(none)>
2011-03-12 15:11:06 +0800

    added terminal option for using improved opponent model

---
+++ ai/me/saac/i/ai/ImprovedOpponentModel.java
@@ -0,0 +1,28 @@
+package me.saac.i.ai;
+
+import java.util.ArrayList;
+
+import me.saac.i.ai.GameState.Action;
+
+public class ImprovedOpponentModel implements OpponentModel {
+
+	@Override
+	public double[] actionProbabilities(ArrayList<Action> history) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void input(ArrayList<Action> history, int handStrength) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double winPossibility(ArrayList<Action> history,
+			int playerHandStrength) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
